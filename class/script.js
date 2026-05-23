d4fea4b833595e03d216464bc61d7a679a301adf
Ankit Kumar Nayak <ankitkumarnayak006@gmail.com>
2026-05-24 01:25:19 +0530

    all class work

---
+++ class/script.js
@@ -0,0 +1,23 @@
+console.log("Ankit");
+console.warn("This is warning");
+console.error("this is array");
+console.table(["ankit"]);
+
+var a = 60;
+console.log(a);
+var b = 20;
+
+console.log("The value of a & b = ", a, b);
+
+console.log(a - b);
+console.log(a * b);
+console.log(a + b);
+
+var c = "hello ankit";
+console.log(c);
+
+var d = 10n;
+
+// alert("are you sure")
+var ans = confirm("Are you 18");
+console.log(ans);
